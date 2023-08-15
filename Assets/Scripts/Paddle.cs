f20f0263b0c4216e01bc584dd6826c2f1e85ae8a
Hamed <hamednourani1026@gmail.com>
2023-08-15 16:56:34 +0330

    Add vertical move behaviour to paddle

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+    private const float MIN_Y_POS = -3.93f;
+    private const float MAX_Y_POS = 3.93f;
+    
     [SerializeField] private float _verticalSpeed;
     
     
@@ -14,11 +17,14 @@ public class Paddle : MonoBehaviour
 
     void Update()
     {
-        
+        VerticalMove();
     }
 
     private void VerticalMove()
     {
+        var verticalMovement = Input.GetAxis("Vertical") * _verticalSpeed * Time.deltaTime;
         
+        transform.position = new Vector3(transform.position.x, 
+            Mathf.Clamp(transform.position.y + verticalMovement, MIN_Y_POS, MAX_Y_POS), transform.position.z);
     }
 }
