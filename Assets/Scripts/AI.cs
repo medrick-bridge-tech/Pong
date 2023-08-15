0b1b80c4cc093dc0336b2dcb758ec94ba76a4b87
Hamed <hamednourani1026@gmail.com>
2023-08-15 22:18:29 +0330

    Implement AI behaviour

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class AI : MonoBehaviour
 {
-    [SerializeField] private float _verticalSpeed;
+    private const float MIN_Y_POS = -3.93f;
+    private const float MAX_Y_POS = 3.93f;
     
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float _verticalSpeed;
+    [SerializeField] private Ball _ball;
+    
 
-    void Update()
+    private void Update()
     {
-        
+        KeepTrackOfBall();
     }
 
     private void KeepTrackOfBall()
     {
-        
+        var targetY = _ball.transform.position.y;
+        targetY = Mathf.Clamp(targetY, MIN_Y_POS, MAX_Y_POS);
+        var step = _verticalSpeed * Time.deltaTime;
+        var targetPosition = new Vector3(transform.position.x, targetY, transform.position.z);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
     }
 }
