d18d5771e67d1063127151c604ae1a0ee7270438
Hamed <hamednourani1026@gmail.com>
2023-08-15 18:21:22 +0330

    Make ball move

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,21 +4,35 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    [SerializeField] private Vector2 _startingSpeed;
-    
-    
+    [SerializeField] private float _startingSpeed;
+
+    private bool _gameHasStarted;
+    private float _randomX;
+    private float _randomY;
+    private Vector2 _randomVector;
+    private Rigidbody2D _ballRigidbody;
+
+
     void Start()
     {
-        
+        _randomX = Random.Range(-1f, 1f);
+        _randomY = Random.Range(-1f, 1f);
+        _randomVector = new Vector2(_randomX, _randomY).normalized;
+
+        _ballRigidbody = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+        if (!_gameHasStarted && Input.GetKeyDown(KeyCode.Space))
+        {
+            StartMoving();
+        }
     }
 
     private void StartMoving()
     {
-        
+        _gameHasStarted = true;
+        _ballRigidbody.velocity = _randomVector * _startingSpeed;
     }
 }
