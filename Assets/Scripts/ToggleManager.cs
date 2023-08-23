90141e9e9c1b450d5fd820bf36d8eefba248d822
Codidodio <Codidodido@gmail.com>
2023-08-23 12:29:36 +0330

    Fix Player Prefab

---
+++ Assets/Scripts/ToggleManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class ToggleManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject obstacleManager;
+        
+        
+        
+        void 
+    }
+}
