375dd130ef78e6c4f6ade5cf0b8f74a904d74f7a
Codidodio <Codidodido@gmail.com>
2023-08-23 23:18:20 +0330

    Add goal particle VFX to game

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Score : MonoBehaviour
@@ -11,28 +12,33 @@ public class Score : MonoBehaviour
     [SerializeField] private GameManager _gameManager;
     [SerializeField] private ScoreManager _scoreManager;
     [SerializeField] private Ball _ball;
-    
+    [SerializeField] private GameObject goalVFX;
     
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Ball")
         {
+            Color color;
             var ballRigidbody = other.gameObject.GetComponent<Rigidbody2D>();
-            
+               
             if (ballRigidbody.velocity.x < 0f)
             {
                 _playerScore++;
                 _scoreManager.playerScore++;
                 _scoreManager.UpdateScoreboard();
+                color = Color.blue;
             }
             else
             {
                 _aiScore++;
                 _scoreManager.aiScore++;
                 _scoreManager.UpdateScoreboard();
+                color = Color.red;
             }
-            
+
+            GameObject vfx = Instantiate(goalVFX, ballRigidbody.transform.position, quaternion.identity);
+            Destroy(vfx,1f);
             _gameManager.GameState = GameStates.ReadyToStart;
             _ball.ResetPosition();
             _ball.StopMoving();
